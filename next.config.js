6c15a524fad434757079a82d77d6d8fe4685540d
Juan Manuel Scasso <juan@selfdecode.com>
2021-10-22 10:45:08 -0300

    config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+import path from "path";
+
 module.exports = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 }
