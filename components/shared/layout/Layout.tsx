c4e4f339c35de261c9ab69377dd9fd083d73aaca
Juan Manuel Scasso <juan@selfdecode.com>
2021-10-26 11:56:52 -0300

    add meta

--- components/shared/layout/Layout.tsx
+++ components/shared/layout/Layout.tsx
@@ -41,17 +41,6 @@ const Layout = ({ children }: LayoutChildren): JSX.Element => {
     <>
       <Head>
         <title>Velocity</title>
-        <meta name="viewport" content="initial-scale=1.0, width=300" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&display=swap"
-          rel="stylesheet"
-        />
       </Head>
       {renderSwitch(requestStatus)}
     </>
---
+++ pages/_document.tsx
@@ -0,0 +1,44 @@
+// eslint-disable-next-line @next/next/no-document-import-in-page
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    return await Document.getInitialProps(ctx);
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name="viewport" content="initial-scale=1.0, width=300" />
+          <meta
+            name="description"
+            content="Official website of Lidnat Software LLC"
+          />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
